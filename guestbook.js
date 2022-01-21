d8fb464f18f53698e657893ac98000ed07ca929b
vivekpillai99 <46638144+vivekpillai99@users.noreply.github.com>
2022-01-21 15:43:02 -0700

    added api

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://67f011f5.us-south.apigw.appdomain.cloud/serverless-final/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
